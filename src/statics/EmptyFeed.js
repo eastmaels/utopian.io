8e5d832c5afd8be212f48006d23bd380846a83b1
Diego Pucci <me@diegopucci.com>
2017-10-14 15:40:15 +0200

    done

--- src/statics/EmptyFeed.js
+++ src/statics/EmptyFeed.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
+import './EmptyFeed.less';
+
 const EmptyFeed = () =>
-  (<div className="text-center">
+  (<div className="EmptyFeed">
     <h3>
-      No Contribution Reports for this project yet. <b>Be the first!</b>{' '}
+      No contributions found. {' '}
       <Link to="/write">
         Write a Contributor Report
       </Link>
