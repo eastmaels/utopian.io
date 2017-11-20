d0cca1322b3bf7627c6b5620ebc50e8fa6bebb58
mooncryption <mooncryption@gmail.com>
2017-11-20 14:54:47 -0800

    Blog Posts Now Show up in User Profile
    https://utopian.io/@mooncryption
    
    for example, ^

--- src/user/UserFeed.js
+++ src/user/UserFeed.js
@@ -65,7 +65,7 @@ export default class UserProfileFeed extends React.Component {
         isFetching={isFetching}
         hasMore={hasMore}
         loadMoreContent={loadMoreContentAction}
-        showBlogs={false}
+        showBlogs={true}
       />
     );
   }
