cc2880794cf95d4ba20640356e793d5af592bdb7
samrg472 <samrg472@gmail.com>
2017-11-26 15:52:13 -0800

    Use a single callback function for the two servers

---
+++ server/auth-callback.js
@@ -0,0 +1,14 @@
+function authCallback(req, res) {
+  const accessToken = req.query.access_token;
+  const expiresIn = req.query.expires_in;
+  const state = req.query.state;
+  const next = state && state[0] === '/' ? state : '/';
+  if (accessToken && expiresIn) {
+    res.cookie('access_token', accessToken, { maxAge: expiresIn * 1000 });
+    res.redirect(next);
+  } else {
+    res.status(401).send({ error: 'access_token or expires_in Missing' });
+  }
+}
+
+module.exports = authCallback;
--- server/auth-server.js
+++ server/auth-server.js
@@ -1,23 +1,9 @@
-/**
- * Express.js server used for handling callback request after loging in.
- */
+const authCallback = require('./auth-callback');
 
 const express = require('express');
-
 const app = express();
 
-app.get('/callback', (req, res) => {
-  const accessToken = req.query.access_token;
-  const expiresIn = req.query.expires_in;
-  const state = req.query.state;
-  const next = state && state[0] === '/' ? state : '/';
-  if (accessToken && expiresIn) {
-    res.cookie('access_token', accessToken, { maxAge: expiresIn * 1000 });
-    res.redirect(next);
-  } else {
-    res.status(401).send({ error: 'access_token or expires_in Missing' });
-  }
-});
+app.get('/callback', authCallback);
 
 const server = app.listen(3001, 'localhost', () => {
   const host = server.address().address;
