d96f0175ae0aefd69450e766260b7d7264bab0f1
mooncryption <mooncryption@gmail.com>
2017-12-10 18:58:48 -0800

    Final Sponsorship Fix

--- src/statics/Sponsors.js
+++ src/statics/Sponsors.js
@@ -58,7 +58,7 @@ class Sponsors extends React.PureComponent {
 
   generateSteemURI(from, amount) {
     from = from.replace("@", "");
-    const amtVests = (parseFloat(amount) / parseFloat(this.state.total_vesting_fund_steem)) * parseFloat(this.state.total_vesting_shares);
+    const amtVests = (10 * (parseFloat(amount) / parseFloat(this.state.total_vesting_fund_steem))) * parseFloat(this.state.total_vesting_shares);
     const preSuffix = [
       [
         "delegate_vesting_shares",
