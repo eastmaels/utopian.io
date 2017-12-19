b7687e116ae65bd0f3477e94d6becd2b49afbba0
Diego Pucci <me@diegopucci.com>
2017-12-19 14:19:22 +0100

    rules update #5

--- src/statics/Rules.js
+++ src/statics/Rules.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Rules } from '../components/Rules';
-import './Rules.less';
+import React from "react";
+import {Rules} from "../components/Rules";
+import "./Rules.less";
 
 export default (props) =>
-  (<div className="main-panel help-section">
+  (<div className="main-panel rules-section">
     <div className="container text-center my-5">
-    <h2>
+      <h2>
         See our FAQ
       </h2>
       <p>
@@ -13,11 +13,35 @@ export default (props) =>
       </p>
       <br />
       <h2>
+        No Commercial/Copyrighted Materials
+      </h2>
+      <p>
+        No commercial or copyrighted material should ever be used in a contribution.</p>
+      <ul>
+        <li>
+          Only Creative Commons for Images and Videos can be accepted.
+        </li>
+        <li>
+          You retain full responsibility if using copyrighted or commercial materials you are not allowed to use.
+        </li>
+      </ul>
+      <br />
+      <h2>
         Check the Github Repository
       </h2>
       <p>
-        The linked Github repository <b>must be the correct one</b> and must actually contain code on Github. If this rule is not met, the contribution won't be accepted. A moderator can request for a change. The repository can be updated by editing the contribution.
-      </p>
+        The linked Github repository <b>must be the correct one</b> and must actually contain code on Github. If this rule is not met, the contribution won't be accepted. A moderator can request for a change. The repository can be updated by editing the contribution.</p>
+      <ul>
+        <li>
+          Contributions for un-official repositories will only be accepted if present in <a href="https://docs.google.com/spreadsheets/d/1RMQyfqKRmgeIWPM80h31hS1NdLxqLJ6qy3I5ucNGuuc/edit?usp=sharing">Utopian un-official repos whitelist</a>
+        </li>
+        <li>
+          Contributions on <b>official</b> repositories that are just mirrors are always accepted.
+        </li>
+        <li>
+          Contributions on repositories that had no updates for <b>longer than 1 year</b>, will be rejected.
+        </li>
+      </ul>
       <br />
       <h2>
         Check the Category
@@ -26,19 +50,45 @@ export default (props) =>
         The contribution must be actually related to the category where it has been submitted. A moderator can request to change the category. The category can be updated by editing the contribution. See the available categories below.
       </p>
       <br />
-      <h2>Contributions MUST BE Informative and Narrative</h2>
+      <h2>Contributions should be Formal and Professional</h2>
+      <p>A moderator may reject a contribution if it is not written in a formal and professional style.</p>
+      <ul>
+        <li>
+          The writing style should be formal not informal.
+        </li>
+        <li>
+          The writing style should be professional.
+        </li>
+        <li>
+          Quality of the images and videos should be high. Low res images and videos will be rejected.
+        </li>
+        <li>A contribution with bad grammar may be rejected if the contents may be really hard to understand.</li>
+      </ul>
+      <h2>Contributions should be Informative and Narrative</h2>
       <p>
-        The contribution must contain as much detail as possible and have some graphical content in it (images, charts, videos, etc) where applicable. The length of the body of the contribution must be enough to give every possible detail about the submitted contribution. If this rule is not met, the contribution won't be accepted in Utopian. A moderator can request for changes before the contribution will be accepted.
-      </p>
+        The contribution must contain as much detail as possible and have some graphical content in it (images, charts, videos, etc) where applicable. The length of the body of the contribution should be enough to give every possible detail about the submitted contribution. If this rule is not met, the contribution won't be accepted in Utopian. A moderator can request for changes before the contribution will be accepted. </p>
+      <b>Some categories do not have to bind to this rule:</b>
+      <ul>
+        <li>Contributions on the Development category do not have to be narrative as long as the contributor gives enough details to evaluate the work done.</li>
+        <li>Contributions in the Bug-Hunting category do not have to be narrative as long as the contributors gives enough details to reproduce the bug.</li>
+      </ul>
       <br />
       <h2>Contributions should be in English</h2>
-      <p>
-        Contributions should be in plain English and fully understandable. The only accepted exceptions are: <br />1. The effort to document an Open Source project in a specific language <br />2. Contributions under the Translations category <br />3. Open Source projects that are meant to be for specific languages only</p>
+      Contributions should be in plain English and fully understandable. The only accepted exceptions are:
+      <ul>
+        <li>Contributions under the Tutorials and Video Tutorials Category.</li>
+        <li>Open Source projects that are meant to be for specific languages only.</li>
+      </ul>
       <br />
       <h2>Never Submitted Before</h2>
       <p>
-        Contributions must be unique. Users must first check if the same or very similar contribution has been submitted before. A moderator will check if the contribution has been reported before and won't accept it in the Utopian feed again if so.
-      </p>
+        Contributions must be unique. Users must first check if the same or very similar contribution has been submitted before. A moderator will check if the contribution has been reported before and won't accept it in the Utopian feed again if so.</p>
+      <ul>
+        <li>Same contributions will never be accepted in Utopian twice.</li>
+        <li>Contents already shared on the Steem blockchain in general may still be rejected if submitted in Utopian.</li>
+        <li>Contents already shared anywhere else may still be rejected if submitted in Utopian.</li>
+        <li>Same contents already shared before in Utopian or anywhere else by different users may still be rejected if a moderator recognises plagiarism.</li>
+      </ul>
       <br />
       <h2>Provides Proof Of Work</h2>
       <p>
@@ -62,13 +112,13 @@ export default (props) =>
       <br />
       <h2>Moderators</h2>
       <p>For moderators, check <em><a href="https://utopian.io/welcome-moderator">Welcome Moderator</a></em> for guidelines on how to effectively moderate on Utopian.</p><br/>
+      <p>A moderator should always give at least one opportunity to fix/edit a post, and never reject and hide it on the first sight.</p>
       <p>For contributors, remember that Utopian Moderators reserve the right to a) verify acceptable posts, b) deliberate and review pending posts, and c) flag/hide posts that do not follow the rules.
-      Additionally, moderators reserve the right to apply temporal or permanent bans who have shown a history of consistently disobeying the rules. <br/>
-      Lastly, the Utopian Bot's vote on your post is not based on human input as of now, and is unrelated to the moderator of your post. If you believe your post got a low vote, please check the 
-      &nbsp;<code>#human-curators</code> channel on our Discord server.</p>
+        Additionally, moderators reserve the right to apply temporal or permanent bans who have shown a history of consistently disobeying the rules. <br/>
+        </p>
       <h2>Categories Specific Rules</h2>
       <br />
-      <Rules inEditor={false} type="ideas" /> 
+      <Rules inEditor={false} type="ideas" />
       <br />
       <Rules inEditor={false} type="sub-projects" />
       <br />
