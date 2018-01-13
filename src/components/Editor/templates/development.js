04561c0b5037744d2384c8c8bd4051364e7bd17c
mkt <kontakt@markus-kottlaender.de>
2018-01-13 15:54:43 +0100

    update development editor template

--- src/components/Editor/templates/development.js
+++ src/components/Editor/templates/development.js
@@ -1,14 +1,30 @@
 export const development = () => {
   return (
     `
-#### Details
-Write here all the details about the code contribution you are submitting. Details of new features / enhancements, paste screenshots or recordings if applicable.    
+*Please use the following structure, depending on the type of your contribution.*
 
-#### Components
-Write here the components/parts/sections of the software that are affected by your changes.
+### Bug Fixes
+- What was the issue(s)?
+Describe briefly and add screenshots/recordings, if applicable.
+Provide links to related issues on GitHub, that you fixed, if applicable.
+- What was the solution?
+Describe briefly how you fixed the issue(s).
 
-#### Related issues
-Paste here the related Github issues your code contribution has fixed, if applicable.
+### New Features
+- What feature(s) did you add?
+Describe briefly and add screenshots/recordings if applicable.
+- How did you implement it/them?
+Link to relevant lines in the code on GitHub and explain briefly what you added/changed.
+
+### New Projects
+- What is the project about?
+Explain in great detail what your project is about and briefly describe already existing features.
+- Technology Stack
+Briefly list the used technologies and languages and other technical requirements.
+- Roadmap
+Write about your future plans related to the project.
+- How to contribute?
+Provide detailed information for contributors about you and how they can get in touch with you.
     `
   )
 }
