ce18a1445edfc42a4a1559f65a560079a98825bf
East <eastmaels@gmail.com>
2018-03-02 07:34:34 +0800

    added project icon to link to utopian project page when repo inline edit is enabled

--- src/components/Story/Contribution.js
+++ src/components/Story/Contribution.js
@@ -155,12 +155,15 @@ const Contribution = ({
       {repository && platform && id ? <span>
         {' '} <b>&middot;</b> {'  '} <a href={`https://github.com/${repository.full_name}`}><Icon type='github' /></a>
         { isModerator && !post.reviewed ?
-          <InlineRepoEdit
+          <span>
+            {' '} <b>&middot;</b> {'  '} <Link to={`/project/${repository.full_name}/${platform}/${id}/all`}><Icon type="folder-open" /></Link>
+            <InlineRepoEdit
               value={parsedRepoName(repository.owner.login, repository.name)}
               post={post}
               moderatorAction={moderatorAction}
               user={user}
-          /> :
+            />
+          </span> ) :
           <Link to={`/project/${repository.full_name}/${platform}/${id}/all`}>{parsedRepoName(repository.owner.login, repository.name)}</Link>
         }
       </span> : null}
