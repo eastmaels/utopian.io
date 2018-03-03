7f3474f4ae53e68dd8c0f489eb8633fdc8ff2dc9
azarusx <azarusx@gmail.com>
2018-03-03 23:10:02 +0100

    remove unused thing

--- src/statics/TOS.js
+++ src/statics/TOS.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import { FormattedMessage } from 'react-intl';
 import { getStats } from '../actions/stats';
 import './TOS.less';
 
