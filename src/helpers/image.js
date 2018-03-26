b8b4b375f6413a183221839926fe6e391f4af420
Nico Wehmöller <nico.wehmoeller@t-online.de>
2018-03-26 14:24:27 +0200

    fix image proxy

--- src/helpers/image.js
+++ src/helpers/image.js
@@ -2,10 +2,14 @@ const IMG_PROXY = 'https://steemitimages.com/0x0/';
 const IMG_PROXY_PREVIEW = 'https://steemitimages.com/600x800/';
 
 export const getProxyImageURL = (url, type) => {
-  if (type === 'preview') {
-    return `${IMG_PROXY_PREVIEW}${url}`;
+  if (url.startsWith("https://cdn.utopian.io")) {
+    return url;
+  } else{
+    if (type === 'preview') {
+      return `${IMG_PROXY_PREVIEW}${url}`;
+    }
+    return `${IMG_PROXY}${url}`;
   }
-  return `${IMG_PROXY}${url}`;
 };
 
 export default null;
